75798fd17b2debf6510dd8b3e87a66c8bff6833b
rm23nm <rm23nm@gmail.com>
2026-05-25 06:44:49 +0700

    comit

--- public/check_cols.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$columns = \Illuminate\Support\Facades\Schema::getColumnListing('users');
-echo "Columns in local users table:\n" . implode(", ", $columns) . "\n";
