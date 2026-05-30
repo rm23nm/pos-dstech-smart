f4db84fae528f1d729c41a1cb807feffa846e7fd
rm23nm <rm23nm@gmail.com>
2026-05-30 23:08:47 +0700

    comit

---
+++ update2.sql
@@ -0,0 +1,4 @@
+
+    UPDATE permission SET Link = 'customerdisplay' WHERE PermissionName = 'Queue Antrian FNB';
+    UPDATE permission SET Link = 'fpenjualan/custdisplay' WHERE PermissionName = 'Customer Display POS';
+    
